eb86f9437a9d6636e5d38333127268e46d83f721
Marcin Włodarski <mwlodarski@atlassian.com>
2020-05-21 10:20:49 +0200

    fixed TZ issue in script

--- bin/sfx.sh
+++ bin/sfx.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-signalflow --api-endpoint https://api.us1.signalfx.com --stream-endpoint https://stream.us1.signalfx.com --timezone `date +%Z` $@
+signalflow --api-endpoint https://api.us1.signalfx.com --stream-endpoint https://stream.us1.signalfx.com --timezone `readlink /etc/localtime|cut -f6- -d"/" ` $@
